28f053f8d071d3b7352e08a1b6d6051609c4a0e0
박주현 <bagjuhyeon@bagjuhyeon-ui-MacBookPro.local>
2025-01-12 02:53:54 +0900

    func1

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from sqlalchemy.orm import Session
 from app.database.conn import engine, session
 from app.database import models
+from utils.func1 import score1
 
 models.base.metadata.create_all(bind=engine)
 app = FastAPI()
@@ -16,3 +17,9 @@ def get_db():
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/score1")
+async def func1():
+    sample_data = [10, 30, 90, -50, 70]
+    return score1(sample_data)
---
+++ backend/utils/func1.py
@@ -0,0 +1,14 @@
+# 선행 score api
+#sample_data = pd.read_csv("sample_data.csv")
+
+# roll 값이 70 이상이면 True(기울어짐)), 아니면 False
+def is_fallen(sample_data: list) -> int:
+    critical_angle = 70
+    return [1 if abs(roll) > critical_angle else 0 for roll in sample_data]
+
+# 위의 roll 값 기준으로 점수 계산
+def score1(sample_data: list) -> int:
+    weight = 10
+    return list(map(lambda x: x * weight, is_fallen(sample_data)))
+
+
