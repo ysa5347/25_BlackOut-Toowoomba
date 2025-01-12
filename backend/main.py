6e91a8695c0773000e9d523bc76a943da01dfa3d
ysa5347 <ysa5347@gmail.com>
2025-01-12 09:31:34 +0900

    FEAT; backend models, tests, 1st API create(1)

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from app.database.conn import engine, session
 from app.database import models
+import json
+import requests
 from utils.func1 import score1
 
 models.base.metadata.create_all(bind=engine)
@@ -17,6 +20,32 @@ def get_db():
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# POST /rolled; body: {bid: int, uid: int}
+@app.post('/rolled')
+async def rolled(requests, db: Session = get_db()):
+    requests = json.loads(requests)
+    bicycle = db.query(models.Bicycle).filter(models.Bicycle.bid == requests['body'].bid).first()
+    user = db.query(models.User).filter(models.User.uid == requests['body'].uid).first()
+    if bicycle is None:
+        raise HTTPException(status_code=404, detail="Bicycle not found")
+    if bicycle.status == "rolled":
+        pass # 이 부분에 임베디드와 통신해서 올바로 세워졌는지 확인하는 코드 추가
+        bicycle.status = "available"
+        db.commit()
+        if not user.daylimit:
+            return {"message": f"Bicycle {bicycle.bid}is available now"}
+        give_coupon(user.uid, name="ThankToRolling", code="THANKYOUTOROLL", value=10)
+        user.daylimit -= 1
+        db.commit()
+        return {"message": f"Bicycle {bicycle.bid}is available now"}
+        
+
+def give_coupon(uid: int, code: str, name: str, value: int, type: str = "percent"):
+    db = session()
+    coupon = models.Coupons(uid=uid, name=name, description="Thank you", code=code, value=value, type=type)
+    db.add(coupon)
+    db.commit()
 
 
 @app.get("/score1")
--- backend/utils/func1.py
+++ backend/utils/func1.py
@@ -2,12 +2,12 @@
 #sample_data = pd.read_csv("sample_data.csv")
 
 # roll 값이 70 이상이면 True(기울어짐)), 아니면 False
-def is_fallen(sample_data: list) -> int:
+def is_fallen(sample_data: list) -> list[int]:
     critical_angle = 70
     return [1 if abs(roll) > critical_angle else 0 for roll in sample_data]
 
 # 위의 roll 값 기준으로 점수 계산
-def score1(sample_data: list) -> int:
+def score1(sample_data: list) -> list[int]:
     weight = 10
     return list(map(lambda x: x * weight, is_fallen(sample_data)))
 
